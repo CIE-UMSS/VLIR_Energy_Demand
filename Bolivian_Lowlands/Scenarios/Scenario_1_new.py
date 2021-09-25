3a9c7a034ac6ff668c7734597daf4696f62ef671
ClaudiaLSS <79788249+ClaudiaLSS@users.noreply.github.com>
2021-09-25 16:51:11 -0400

    Add files via upload
    Specific load and scenarios for Bolivian lowlands

---
+++ Bolivian_Lowlands/Scenarios/Scenario_1_new.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 21 09:46:00 2021
+
+@author: Clau
+"""
+
+'''
+Paper: Energy sufficiency, lowlands.
+SCENARIO 1
+'''
+
+from core import User, np
+User_list = []
+
+#Defining users
+H1 = User("low income", 120) 
+User_list.append(H1)
+
+H2 = User("high income", 6)
+User_list.append(H2)
+
+Public_lighting = User("Public lighting ", 2)
+User_list.append(Public_lighting)
+
+R = User("Restaurant", 1)
+User_list.append(R)
+
+GS = User("Grocery Store 1", 1)
+User_list.append(GS)
+
+EB = User("Entertainment Business", 1)
+User_list.append(EB)
+
+WS = User("Workshop", 1)
+User_list.append(WS)
+
+
+#Appliances
+
+#Low Income Households
+H1_indoor_bulb = H1.Appliance(H1,3,7,2,120,0.2,10)
+H1_indoor_bulb.windows([1082,1440],[0,30],0.35)
+
+H1_outdoor_bulb = H1.Appliance(H1,1,13,2,600,0.2,10)
+H1_outdoor_bulb.windows([0,330],[1082,1440],0.35)
+
+H1_TV = H1.Appliance(H1,1,60,3,90,0.1,5)
+H1_TV.windows([750,840],[1082,1440],0.35,[0,30])
+
+H1_Antenna = H1.Appliance(H1,1,8,3,90,0.1,5)
+H1_Antenna.windows([750,840],[1082,1440],0.35,[0,30])
+
+H1_Phone_charger = H1.Appliance(H1,2,2,1,300,0.2,5)
+H1_Phone_charger.windows([1080,1440],[0,0],0.35)
+
+#High income households
+H2_indoor_bulb = H2.Appliance(H2,4,7,2,120,0.2,10)
+H2_indoor_bulb.windows([1082,1440],[0,30],0.35)
+         
+H2_outdoor_bulb = H2.Appliance(H2,2,13,2,600,0.2,10)
+H2_outdoor_bulb.windows([0,330],[1082,1440],0.35)
+
+H2_TV = H2.Appliance(H2,2,60,2,120,0.1,5)
+H2_TV.windows([1082,1440],[0,60],0.35)
+
+H2_DVD = H2.Appliance(H2,1,8,2,40,0.1,5)
+H2_DVD.windows([1082,1440],[0,60],0.35)
+
+H2_Antenna = H2.Appliance(H2,1,8,2,80,0.1,5)
+H2_Antenna.windows([1082,1440],[0,60],0.35)
+
+H2_Radio = H2.Appliance(H2,1,36,2,60,0.1,5)
+H2_Radio.windows([390,450],[1082,1260],0.35)
+
+H2_Phone_charger = H2.Appliance(H2,4,2,2,300,0.2,5)
+H2_Phone_charger.windows([1110,1440],[0,30],0.35)
+
+H2_Freezer = H2.Appliance(H2,1,200,1,1440,0,30, 'yes',2)
+H2_Freezer.windows([0,1440],[0,0])
+H2_Freezer.specific_cycle_1(5,15,200,15)
+H2_Freezer.specific_cycle_2(200,10,5,20)
+H2_Freezer.cycle_behaviour([480,1200],[0,0],[0,479],[1201,1440])
+
+H2_Mixer = H2.Appliance(H2,1,50,3,30,0.1,1, occasional_use = 0.33)
+H2_Mixer.windows([420,450],[660,750],0.35,[1020,1170])
+
+H2_Fan = H2.Appliance(H2,1,171,1,220,0.27,60)
+H2_Fan.windows([720,1080],[0,0])
+
+H2_Laptop = H2.Appliance(H2,1,70,1,90,0.3,30)
+H2_Laptop.windows([960,1200],[0,0])
+
+#Public lighting
+Public_lighting_lamp_post = Public_lighting.Appliance(Public_lighting,12,40,2,310,0,300, 'yes', flat = 'yes')
+Public_lighting_lamp_post.windows([0,362],[1082,1440],0.1)
+
+#Grocery Store
+GS_indoor_bulb = GS.Appliance(GS,2,7,2,120,0.2,10)
+GS_indoor_bulb.windows([1107,1440],[0,30],0.35)
+
+GS_outdoor_bulb = GS.Appliance(GS,1,13,2,600,0.2,10)
+GS_outdoor_bulb.windows([0,330],[1107,1440],0.35)
+
+GS_freezer = GS.Appliance(GS,1,200,1,1440,0,30,'yes',3)
+GS_freezer.windows([0,1440],[0,0])
+GS_freezer.specific_cycle_1(200,20,5,10)
+GS_freezer.specific_cycle_2(200,15,5,15)
+GS_freezer.specific_cycle_3(200,10,5,20)
+GS_freezer.cycle_behaviour([480,1200],[0,0],[300,479],[0,0],[0,299],[1201,1440])
+
+GS_Radio = GS.Appliance(GS,1,36,2,60,0.1,5)
+GS_Radio.windows([390,450],[1140,1260],0.35)
+
+#Restaurant
+R_indoor_bulb = R.Appliance(R,2,7,2,120,0.2,10)
+R_indoor_bulb.windows([1107,1440],[0,30],0.35)
+
+R_Blender = R.Appliance(R,1,350,2,20,0.375,5)
+R_Blender.windows([420,480],[720,780],0.5)
+
+R_freezer = R.Appliance(R,1,200,1,1440,0,30,'yes',3)
+R_freezer.windows([0,1440],[0,0])
+R_freezer.specific_cycle_1(200,20,5,10)
+R_freezer.specific_cycle_2(200,15,5,15)
+R_freezer.specific_cycle_3(200,10,5,20)
+R_freezer.cycle_behaviour([480,1200],[0,0],[300,479],[0,0],[0,299],[1201,1440])
+
+#Entertainment Business
+EB_indoor_bulb = EB.Appliance(EB,2,7,2,120,0.2,10)
+EB_indoor_bulb.windows([1107,1440],[0,30],0.35)
+
+EB_outdoor_bulb = EB.Appliance(EB,1,13,2,600,0.2,10)
+EB_outdoor_bulb.windows([0,330],[1107,1440],0.35)
+
+EB_Stereo = EB.Appliance(EB,1,150,2,90,0.1,5, occasional_use = 0.33)
+EB_Stereo.windows([480,780],[0,0],0.35)
+
+EB_TV = EB.Appliance(EB,1,60,2,120,0.1,5, occasional_use = 0.5)
+EB_TV.windows([480,780],[840,1140],0.2)
+    
+EB_PC = EB.Appliance(EB,1,50,2,210,0.1,10)
+EB_PC.windows([480,780],[840,1140],0.35)
+
+EB_freezer = EB.Appliance(EB,1,200,1,1440,0,30,'yes',3)
+EB_freezer.windows([0,1440],[0,0])
+EB_freezer.specific_cycle_1(200,20,5,10)
+EB_freezer.specific_cycle_2(200,15,5,15)
+EB_freezer.specific_cycle_3(200,10,5,20)
+EB_freezer.cycle_behaviour([480,1200],[0,0],[300,479],[0,0],[0,299],[1201,1440])
+
+WS_indoor_bulb = WS.Appliance(WS,2,7,2,120,0.2,10)
+WS_indoor_bulb.windows([1107,1440],[0,30],0.35)
+
+WS_welding_machine = WS.Appliance(WS,1,5500,1,60,0.5,30,occasional_use = 0.3)
+WS_welding_machine.windows([0,1440],[0,0],0.35)
+
+WS_grinding_machine = WS.Appliance(WS,1,750,1,480,0.125,60)
+WS_grinding_machine.windows([0,1440],[0,0],0.35)
+
+WS_Radio = WS.Appliance(WS,1,36,2,60,0.1,5)
+WS_Radio.windows([390,450],[1140,1260],0.35)
